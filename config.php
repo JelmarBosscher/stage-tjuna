5acd7a5e790631fe2c61ae00fe1b8354d9c28b89
Jelmar Bosscher <jelmar@tjuna.com>
2013-01-23 17:07:00 +0100

    stageverslag
    Signed-off-by: Jelmar Bosscher <jelmar@tjuna.com>

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ return array(
 		'host' => 'localhost',
 		'port' => '3306',
 		'username' => 'root',
-		'password' => '',
+		'password' => 'root',
 		'name' => 'stageverslag',
 		'collation' => 'utf8_general_ci'
 	),
